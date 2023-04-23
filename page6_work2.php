4b5e92cd8a7f10e04b72917d5e30549844700321
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-23 16:08:17 +0200

    whywontitwork

--- page6_work2.php
+++ page6_work2.php
@@ -47,7 +47,7 @@ $data3 = $statement->fetch(PDO::FETCH_ASSOC);
         <a class="X" href="page3_work.php"> X </a>
       </div>
 
-      <div class="background">
+      <div class="colorfond">
         <h1><?php echo $data1["titre_projet"]?></h1>
         <h2><?php echo $data1["description"]?></h2>
         <p><?php echo $data1["explication"]?></p>
