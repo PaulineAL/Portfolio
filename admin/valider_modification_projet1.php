14432db53523d56ec46688cd644006487972cf2f
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-23 19:44:43 +0200

    Update valider_modification_projet1.php

--- admin/valider_modification_projet1.php
+++ admin/valider_modification_projet1.php
@@ -17,7 +17,7 @@ $sql .= "img_projet_1=\"".$img_projet_1."\",";
 $sql .= "img_projet_2=\"".$img_projet_2."\",";
 $sql .= "img_projet_3=\"".$img_projet_3."\",";
 $sql .= "img_projet_4=\"".$img_projet_4."\",";
-// $sql .= "WHERE id_projet =1 ";
+$sql .= "WHERE id_projet =1 ";
 
 $statement = $pdo->query($sql);
 
