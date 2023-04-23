d5e38bd9cdd114ea2f2b53c7ffad3dc954c00017
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-23 18:09:54 +0200

    modif projet

---
+++ admin/valider_modification_projet1.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+		<meta charset="utf-8">
+	<title>Modifier article</title>
+</head>
+<body>
+<?php 
+//récupération des champs venant du formulaire
+$titre_projet=($_POST['titre_projet']);
+$description=$_POST['description'];
+$explication=$_POST['explication'];
+$Img_projet_1=$_POST['Img_projet_1'];
+$Img_projet_2=$_POST['Img_projet_2'];
+$Img_projet_3=$_POST['Img_projet_3'];
+$Img_projet_4=$_POST['Img_projet_4'];
+
+$pdo = require '../model/connect.php';
+
+//Ecriture de la requête 
+$sqlQuery = "UPDATE projet_1 SET 
+titre_projet=:titre_projet,description=:description,explication=:explication,Img_projet_1=:Img_projet_1,Img_projet_2=:Img_projet_2,Img_projet_3=:Img_projet_3,Img_projet_4=:Img_projet_4;
+
+//Préparation 
+$insertprojet = $pdo->prepare($sqlQuery);
+
+//Exécution!
+$insertprojet ->execute ([
+	'titre_projet' => $titre_projet,
+	'description' => $description,
+    'explication' => $explication,
+	'Img_projet_1' => $Img_projet_1,
+    'Img_projet_2' => $Img_projet_2,
+	'Img_projet_3' => $Img_projet_3,
+	'Img_projet_4' => $Img_projet_4
+]);
+	header ("Location:index.php"); //appel de la page index.php 
+?>
+
+</body>
+</html>
