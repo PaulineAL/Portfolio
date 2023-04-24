1d99b97b7fa21c4fd631c81be72b0bd9ea96d14e
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-24 17:24:36 +0200

    Update valider_modification_projet1.php

--- admin/valider_modification_projet1.php
+++ admin/valider_modification_projet1.php
@@ -21,8 +21,6 @@ $sql .= "img_projet_1=\"".$img_projet_1."\", ";
 $sql .= "img_projet_2=\"".$img_projet_2."\", ";
 $sql .= "img_projet_3=\"".$img_projet_3."\", ";
 $sql .= "img_projet_4=\"".$img_projet_4."\" ";
-$sql .= "WHERE id_projet = 1";
-$sql .= "UPDATE projet_1 SET ";
 $sql .= "explication=\"".$explication."\", ";
 $sql .= "WHERE id_projet = 1";
 
