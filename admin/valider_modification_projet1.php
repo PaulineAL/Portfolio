685bbbf34afbaf5834a8789c2072043c4acc43c7
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-24 14:31:27 +0200

    Update valider_modification_projet1.php

--- admin/valider_modification_projet1.php
+++ admin/valider_modification_projet1.php
@@ -5,20 +5,12 @@ if(isset($_POST['modifier'])){
 $titre_projet=$_POST['titre_projet'];
 $description=$_POST['description'];
 $explication=$_POST['explication'];
-$Img_projet_1=$_POST['img_projet_1'];
-$Img_projet_2=$_POST['img_projet_2'];
-$Img_projet_3=$_POST['img_projet_3'];
-$Img_projet_4=$_POST['img_projet_4'];
+$img_projet_1=$_POST['img_projet_1'];
+$img_projet_2=$_POST['img_projet_2'];
+$img_projet_3=$_POST['img_projet_3'];
+$img_projet_4=$_POST['img_projet_4'];
 
-$sql = "UPDATE projet_1 SET ";
-$sql .= "titre_projet=\"".$titre_projet."\",";
-$sql .= "description=\"".$description."\",";
-$sql .= "explication=\"".$explication."\",";
-$sql .= "img_projet_1=\"".$img_projet_1."\",";
-$sql .= "img_projet_2=\"".$img_projet_2."\",";
-$sql .= "img_projet_3=\"".$img_projet_3."\",";
-$sql .= "img_projet_4=\"".$img_projet_4."\",";
-$sql .= "WHERE id_projet =1 ";
+$sql = "UPDATE projet_1 SET titre_projet=$titre_projet, description=$description,explication=$explication,img_projet_1=$img_projet_1,img_projet_2=$img_projet_2,img_projet_3=$img_projet_3, img_projet_4=$img_projet_4 WHERE id_projet = 1 ";
 
 $statement = $pdo->query($sql);
 
