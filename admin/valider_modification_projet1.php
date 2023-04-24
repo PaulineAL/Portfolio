dda722e68d4b9e34673f5e234cbd9101c40ca38d
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-24 15:53:00 +0200

    Update valider_modification_projet1.php

--- admin/valider_modification_projet1.php
+++ admin/valider_modification_projet1.php
@@ -13,15 +13,15 @@ $img_projet_2=$_POST['img_projet_2'];
 $img_projet_3=$_POST['img_projet_3'];
 $img_projet_4=$_POST['img_projet_4'];
 
-$sql = "UPDATE projet_1 SET ";
-$sql .= "titre_projet='".$titre_projet."', ";
-$sql .= "description='".$description."', ";
-$sql .= "explication='".$explication."', ";
-$sql .= "img_projet_1='".$img_projet_1."', ";
-$sql .= "img_projet_2='".$img_projet_2."', ";
-$sql .= "img_projet_3='".$img_projet_3."', ";
-$sql .= "img_projet_4='".$img_projet_4."' ";
-$sql .= "WHERE id_projet = 1";
+$sql = "UPDATE `projet_1` SET ";
+$sql .= "`titre_projet`='".$titre_projet."', ";
+$sql .= "`description`='".$description."', ";
+$sql .= "`explication`='".$explication."', ";
+$sql .= "`img_projet_1`='".$img_projet_1."', ";
+$sql .= "`img_projet_2`='".$img_projet_2."', ";
+$sql .= "`img_projet_3`='".$img_projet_3."', ";
+$sql .= "`img_projet_4`='".$img_projet_4."' ";
+$sql .= "WHERE `id_projet` = 1";
 
 $statement = $pdo->query($sql);
 
