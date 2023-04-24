7f9a21801bcfdff414da33bdada6a2336c2e67c6
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-24 17:25:58 +0200

    Update valider_modification_projet1.php

--- admin/valider_modification_projet1.php
+++ admin/valider_modification_projet1.php
@@ -17,11 +17,11 @@ echo var_dump($_POST);
 $sql = "UPDATE projet_1 SET ";
 $sql .= "titre_projet=\"".$titre_projet."\", ";
 $sql .= "description=\"".$description."\", ";
+$sql .= "explication=\"".$explication."\", ";
 $sql .= "img_projet_1=\"".$img_projet_1."\", ";
 $sql .= "img_projet_2=\"".$img_projet_2."\", ";
 $sql .= "img_projet_3=\"".$img_projet_3."\", ";
 $sql .= "img_projet_4=\"".$img_projet_4."\" ";
-$sql .= "explication=\"".$explication."\", ";
 $sql .= "WHERE id_projet = 1";
 
 $statement = $pdo->query($sql);
