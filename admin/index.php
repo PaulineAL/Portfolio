d8f73d6bbccb856295f7f6e870a888cca0c348f6
PaulineAL <paulineaguiarlalanne@gmail.com>
2023-04-25 18:11:07 +0200

    indexfix2

--- admin/index.php
+++ admin/index.php
@@ -92,7 +92,7 @@ echo "<script>window.location.href='login.php';</script>";
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index.html" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Backoffice</span>
     </a>
